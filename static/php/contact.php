22de22c3a12058b5d564f5003eb3109b3f3fdb69
Guillaume <guillaume@ordi-service.fr>
2023-04-22 08:29:06 +0200

    services

--- static/php/contact.php
+++ static/php/contact.php
@@ -1,26 +1,26 @@
 <?php
 /* Empeche l'acces direct aufichier */
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
-    /* 
+  /* 
            Choix du code d'erreur. Peut-être que 404 est plus sécurisé que 403 
         */
-    // header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-    header('HTTP/1.0 404 Not Found', TRUE, 404);
-    /* Choisir la page de redirection appropriée */
-    die(header('location: /index.html'));
+  // header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+  header('HTTP/1.0 404 Not Found', TRUE, 404);
+  /* Choisir la page de redirection appropriée */
+  die(header('location: /index.html'));
 }
 session_start();
 // Vérifie si le jeton de sécurité est présent(CSRF) et si le champ télephone est vide(HoneyPot)
 if (!$_POST['jeton'] || $_POST['jeton'] !== $_SESSION['jeton'] || $_POST['telephone'] !== "") {
-    // Redirection vers la page remerciement comme si tout c'était bien passé
-    die(header('location: /merci.html'));
+  // Redirection vers la page remerciement comme si tout c'était bien passé
+  die(header('location: /merci.html'));
 }
 
 //  Si trop rapide bot
 $temp = 30;
 if (time() - $_POST['pointeur'] < $temp) {
-    // Redirection vers la page remerciement comme si tout c'était bien passé
-    die(header('location: /merci.html'));
+  // Redirection vers la page remerciement comme si tout c'était bien passé
+  die(header('location: /merci.html'));
 }
 $erreurs = [];
 $maquants = [];
@@ -29,27 +29,27 @@ $requis = ['nom', 'email', 'message'];
 $champ = [];
 foreach ($_POST as $clef => $post) {
 
-    if ($clef == 'email') {
-        $post = filter_var($post, FILTER_SANITIZE_EMAIL);
-        $post = filter_var($post, FILTER_VALIDATE_EMAIL);
-    } else {
-        $post = nettoyage($post);
+  if ($clef == 'email') {
+    $post = filter_var($post, FILTER_SANITIZE_EMAIL);
+    $post = filter_var($post, FILTER_VALIDATE_EMAIL);
+  } else {
+    $post = nettoyage($post);
+  }
+  if ($post != "") {
+    if (in_array($clef, $attendus)) {
+      $champ[$clef] = $post;
     }
-    if ($post != "") {
-        if (in_array($clef, $attendus)) {
-            $champ[$clef] = $post;
-        }
-    }
-    if (in_array($clef, $requis) && $post = "") {
-        $erreurs[$clef] = $post;
-    }
+  }
+  if (in_array($clef, $requis) && $post = "") {
+    $erreurs[$clef] = $post;
+  }
 }
 if (empty($erreurs)) {
-    $entete  = 'MIME-Version: 1.0' . "\r\n";
-    $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-    $entete .= 'From: webmaster@ordi-service.fr' . "\r\n";
-    $entete .= 'Reply-to: ' . $champ['email'];
-    $message = '<h1> Message du site<h1>
+  $entete  = 'MIME-Version: 1.0' . "\r\n";
+  $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+  $entete .= 'From: webmaster@ordi-service.fr' . "\r\n";
+  $entete .= 'Reply-to: ' . $champ['email'];
+  $message = '<h1> Message du site<h1>
     <h1>Message de la page de contact du site ordi-service.fr</h1>
 <dl>
   <dt>Email : </dt>
@@ -63,15 +63,15 @@ if (empty($erreurs)) {
   <dt>IP : </dt>
   <dd>' . $champ['ip'] . '</dd>
 </dl>';
-    $retour = mail('guillaume@ordi-service.fr', 'Envoi depuis page Contact', $message, $entete);
-    if ($retour)
-        // Redirection vers la page remerciement comme si tout c'était bien passé
-        die(header('location: /merci.html'));
+  $retour = mail('guillaume@ordi-service.fr', 'Envoi depuis page Contact', $message, $entete);
+  if ($retour)
+    // Redirection vers la page remerciement comme si tout c'était bien passé
+    die(header('location: /merci.html'));
 }
 function nettoyage($donnee)
 {
-    $donnee = trim($donnee);
-    $donnee = stripslashes($donnee);
-    $donnee = htmlspecialchars($donnee);
-    return $donnee;
+  $donnee = trim($donnee);
+  $donnee = stripslashes($donnee);
+  $donnee = htmlspecialchars($donnee);
+  return $donnee;
 }
